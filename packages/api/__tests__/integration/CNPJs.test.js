8c426d1ac8bd9c353c5326b82106f32b95c1fc09
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2022-12-28 16:35:25 -0300

    feat: implemented settings in the buyers and cnpjs integration test and a new test for create users

--- packages/api/__tests__/integration/CNPJs.test.js
+++ packages/api/__tests__/integration/CNPJs.test.js
@@ -1,19 +1,18 @@
 const { frisbyFunction } = require('./frisby/frisbyFunction');
 
 describe('* CNPJS TESTS *', () => {
-  it('It must be possible to search for all CNPJs in the database.', async () => {
-    const resultAllCNPJs = await frisbyFunction('get', 'cnpjs');
-
-    expect(JSON.parse(resultAllCNPJs._body)).toHaveLength(2);
-  });
-
   it('It should be possible to insert a new CNPJ using the "cnpjs" route with the "post" method.', async () => {
+    const resultAllCNPJsBefore = await frisbyFunction('get', 'cnpjs');
+
     const newCNPJ = {
-      cnpj: '00000000000007',
+      cnpj:
+        JSON.parse(resultAllCNPJsBefore._body).length > 9
+          ? '00000000000000'.slice(0, -2) +
+            `${JSON.parse(resultAllCNPJsBefore._body).length}`
+          : '00000000000000'.slice(0, -1) +
+            `${JSON.parse(resultAllCNPJsBefore._body).length}`,
       companyType: '1',
     };
-
-    const resultAllCNPJsBefore = await frisbyFunction('get', 'cnpjs');
 
     await frisbyFunction('post', 'cnpjs', newCNPJ);
 
@@ -22,5 +21,11 @@ describe('* CNPJS TESTS *', () => {
     expect(JSON.parse(resultAllCNPJsAfter._body)).toHaveLength(
       JSON.parse(resultAllCNPJsBefore._body).length + 1
     );
+  });
+
+  it('It must be possible to search for all CNPJs in the database.', async () => {
+    const resultAllCNPJs = await frisbyFunction('get', 'cnpjs');
+
+    expect(JSON.parse(resultAllCNPJs._body)).not.toHaveLength(0);
   });
 });
