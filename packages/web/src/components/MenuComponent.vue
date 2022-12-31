eea99dc20645beafba8c2bed19e084a7481ca5d7
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2022-12-30 22:36:30 -0300

    feat: skeleton for structuring a dashboard with components.

---
+++ packages/web/src/components/MenuComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="menu-div">
+    <div class="logo-div">Logo</div>
+    <div class="menu">
+      <h1>Menu</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuComponent',
+};
+</script>
+
+<style>
+.menu-div {
+  align-items: center;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  width: 200px;
+}
+
+.menu-div .logo-div {
+  border: 1px solid green;
+  height: 100px;
+  width: 100px;
+}
+</style>
